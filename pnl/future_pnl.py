fix equity_curve calculation issues

pnl/future_pnl.py
@@ -42,8 +42,8 @@ class FuturePnL:
         # 买入合约数 = 固定资金 * 杠杆 / (合约面值 * 开盘价)
         df.loc[open_pos_cond, 'contract_num'] = self._contract_num(df[CANDLE_OPEN_COLUMN])
         # 根据滑点计算实际开仓价格
-        df.loc[open_pos_cond, 'open_pos_price'] = self._price_with_slippage(df[CANDLE_OPEN_COLUMN], df[POSITION_COLUMN],
-                                                                            open_pos_cond)
+        df['open_pos_price'] = self._price_with_slippage(df[CANDLE_OPEN_COLUMN], df[POSITION_COLUMN], open_pos_cond)
+
         # 保证金（扣减手续费）
         df['cash'] = self._cash - df['open_pos_price'] * self._face_value * df['contract_num'] * self._commission
 
@@ -111,10 +111,10 @@ class FuturePnL:
         计算滑点
         """
         # 根据滑点计算实际开仓价格
-        if self._slippage_mode is 'fixed':
+        if self._slippage_mode == 'fixed':
             # 固定滑点
             return np.where(cond, fixed_slippage(price_series, self._slippage), np.NaN)
-        elif self._slippage_mode is 'ratio':
+        elif self._slippage_mode == 'ratio':
             # 比例滑点
             return np.where(cond, ratio_slippage(price_series, direction_series, self._slippage), np.NaN)
         else:

signals/bollinger.py
@@ -12,24 +12,26 @@ def boll_trend(df, **kwargs):
     # 做多
     long_cond1 = df[CANDLE_CLOSE_COLUMN] > df['BBU']  # 收盘价 > 上轨
     long_cond2 = df_s1[CANDLE_CLOSE_COLUMN] <= df_s1['BBU']  # 前收盘价 <= 前上轨
-    df['signal_long'] = np.where(long_cond1 & long_cond2, 1, np.NaN)  # 破上轨，做多
+    df.loc[long_cond1 & long_cond2, 'signal_long'] = 1  # 破上轨，做多
 
     # 平多
     cover_long_cond1 = df[CANDLE_CLOSE_COLUMN] < df['BBM']  # 收盘价 < 均线
-    cover_long_cond2 = df_s1[CANDLE_CLOSE_COLUMN] >= df['BBM']  # 前收 >= 均线
-    df['signal_long'] = np.where(cover_long_cond1 & cover_long_cond2, 0, df['signal_long'])  # 下穿均线，平多
+    cover_long_cond2 = df_s1[CANDLE_CLOSE_COLUMN] >= df_s1['BBM']  # 前收 >= 均线
+    df.loc[cover_long_cond1 & cover_long_cond2, 'signal_long'] = 0  # 下穿均线，平多
 
     # 做空
     short_cond1 = df[CANDLE_CLOSE_COLUMN] < df['BBL']  # 收盘价 < 下轨
     short_cond2 = df_s1[CANDLE_CLOSE_COLUMN] >= df_s1['BBL']  # 前收盘价 >= 前下轨
-    df['signal_short'] = np.where(short_cond1 & short_cond2, -1, np.NaN)  # 破下轨，做空
+    df.loc[short_cond1 & short_cond2, 'signal_short'] = -1  # 破下轨，做空
 
     # 平空
     cover_short_cond1 = df[CANDLE_CLOSE_COLUMN] > df['BBM']  # 收盘价 > 均线
-    cover_short_cond2 = df_s1[CANDLE_CLOSE_COLUMN] <= df['BBM']  # 前收 <= 均线
-    df['signal_short'] = np.where(cover_short_cond1 & cover_short_cond2, 0, df['signal_short'])  # 上穿均线，平空
+    cover_short_cond2 = df_s1[CANDLE_CLOSE_COLUMN] <= df_s1['BBM']  # 前收 <= 均线
+    df.loc[cover_short_cond1 & cover_short_cond2, 'signal_short'] = 0  # 上穿均线，平空
 
     # 合并多空指标
     df = merge_long_short_signal(df, drop_original=True)
+
+    df.drop(columns=['BBU', 'BBM', 'BBL', 'BBB'], inplace=True)
 
     return df