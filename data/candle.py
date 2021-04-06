0b07cc20fa734068f9f5a25c68c223dd5784d081
Shuang Liang <lsuang@gmail.com>
2021-04-06 12:01:19 +0800

    rewrite pipeline and back testing template

---
+++ data/candle.py
@@ -0,0 +1,43 @@
+from datetime import timedelta
+from commons.constants import CANDLE_COLUMNS, CANDLE_DATETIME_COLUMN, CANDLE_OPEN_COLUMN, CANDLE_CLOSE_COLUMN, CANDLE_HIGH_COLUMN, CANDLE_LOW_COLUMN, \
+    CANDLE_VOLUME_COLUMN, POSITION_COLUMN
+
+
+def resample_candle_time_window(df, period='15T', drop_zero_volume='1', drop_zero_open='1'):
+    """合成长时间K线"""
+    period_df = df.resample(rule=period, on=CANDLE_DATETIME_COLUMN, label='left', closed='left').agg(
+        {
+            CANDLE_OPEN_COLUMN: 'first',
+            CANDLE_HIGH_COLUMN: 'max',
+            CANDLE_LOW_COLUMN: 'min',
+            CANDLE_CLOSE_COLUMN: 'last',
+            CANDLE_VOLUME_COLUMN: 'sum',
+        })
+
+    # 去除没有交易的K线
+    if drop_zero_open == '1':
+        period_df.dropna(subset=[CANDLE_OPEN_COLUMN], inplace=True)
+    if drop_zero_volume == '1':
+        period_df = period_df[period_df[CANDLE_VOLUME_COLUMN] > 0]
+
+    period_df.reset_index(inplace=True)
+    df = period_df[CANDLE_COLUMNS]
+
+    return df
+
+
+def skip_days(df, days):
+    """
+    忽略n天的k线
+    """
+    t = df.iloc[0][CANDLE_DATETIME_COLUMN] + timedelta(days=days)
+    return df[df[CANDLE_DATETIME_COLUMN] > t]
+
+
+def add_next_open(df, col='next_open'):
+    """
+    找出下根K线的开盘价
+    """
+    df[col] = df[CANDLE_OPEN_COLUMN].shift(-1)
+    df[col].fillna(value=df[CANDLE_CLOSE_COLUMN], inplace=True)
+    return df
---
+++ run_candle_pipeline.py
@@ -0,0 +1,32 @@
+import argparse
+import timeit
+from commons.io import load_candle_by_ext
+from commons.datetime_utils import str_to_timezone
+from commons.dataframe_utils import filter_candle_dataframe_by_begin_end_offset_datetime
+from pipeline.pipeline import Pipeline
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='run back testing')
+    parser.add_argument('pipeline', help='pipeline template file')
+    parser.add_argument('input', help='history candle file')
+    parser.add_argument('-b', '--begin', help='begin date')
+    parser.add_argument('-d', '--end', help='end date')
+    parser.add_argument('--skip-days', default=0, help='skip days from data begin time, default: 0')
+    parser.add_argument('-z', '--timezone', default='UTC', help='begin, end date timezone(not for candle begin time), default: UTC')
+    args = parser.parse_args()
+
+    # 载入管道
+    pipeline = Pipeline.build_from_template(args.pipeline, verbose=True)
+    # 时区
+    pipeline.context['timezone'] = timezone = str_to_timezone(args.timezone)
+    # 载入数据
+    data = load_candle_by_ext(args.input)
+    data = filter_candle_dataframe_by_begin_end_offset_datetime(data, args.begin, args.end, args.skip_days, tz=timezone)
+
+    # 运行回测
+    start_time = timeit.default_timer()
+    res = pipeline.process(data)
+    end_time = timeit.default_timer()
+    elapse = end_time - start_time
+    print(res)
+    print(f'done, takes {elapse:.2f}s')
